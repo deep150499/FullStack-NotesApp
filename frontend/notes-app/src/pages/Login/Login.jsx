78eb52bd986abb9a3f74f2559091923fb0bbecda
“deep150499” <deepnanavati@icloud.com>
2024-06-20 19:52:30 -0400

    functionality for login page

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -2,23 +2,54 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import { Link } from 'react-router-dom'
 import PasswordInput from "../../components/Input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+          }
+      
+          if (!password) {
+            setError("Please enter the password");
+            return;
+          }
+
+          setError('')
+    }
+
   return (
     <>
         <Navbar/>
 
         <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
 
             <input 
               type="text" 
               placeholder="Email" 
-              className="input-box"  />
+              className="input-box"  
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
 
-            <PasswordInput/>
+            <PasswordInput
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button type="submit" className="btn-primary">Login</button>
 
