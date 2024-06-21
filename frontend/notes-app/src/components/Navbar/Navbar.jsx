54572b42b540a8cea65227d69018a60d1787990e
“deep150499” <deepnanavati@icloud.com>
2024-06-21 10:45:15 -0400

    Search Bar Component

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,51 @@
-import React from 'react'
-import ProfileInfo from '../Cards/ProfileInfo'
+import React, { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+  const isToken = localStorage.getItem("token");
 
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onLogout = () => {
-        localStorage.clear();
-        navigate("/login");
-      };
+  const onLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  const handleSearch = () => {
+    if(searchQuery){
+      onSearchNote(searchQuery)
+    }
+  };
+
+  const onClearSearch = ()=>{
+    handleClearSearch()
+    setSearchQuery("")
+  }
 
   return (
-    <div>
-          <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-                <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-                <ProfileInfo userInfo={userInfo} onLogout={onLogout}/>
-          </div>
-    </div>
-  )
-}
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-export default Navbar
+      {isToken && (
+        <>
+          <SearchBar
+            value={searchQuery}
+            onChange={({ target }) => {
+              setSearchQuery(target.value);
+            }}
+            handleSearch={handleSearch}
+            onClearSearch={onClearSearch}
+          />
+
+          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
