f5e3be62a9ae49412bfa02f42ebf6dc4271d287e
“deep150499” <deepnanavati@icloud.com>
2024-06-25 09:40:21 -0400

    update isPinned  API

--- backend/index.js
+++ backend/index.js
@@ -190,6 +190,36 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     }
   });
 
+// Update isPinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+  
+    try {
+      const note = await Note.findOne({ _id: noteId, userId: user._id });
+  
+      if (!note) {
+        return res.status(404).json({ error: true, message: "Note not found" });
+      }
+  
+      note.isPinned = isPinned;
+  
+      await note.save();
+  
+      return res.json({
+        error: false,
+        note,
+        message: "Note pinned status updated successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal Server Error",
+      });
+    }
+  });
+
   app.listen(3000);
 
 module.exports = app;
