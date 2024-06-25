efbbdfc39fce5d4cb610cae212c19681432cb593
“deep150499” <deepnanavati@icloud.com>
2024-06-25 09:58:43 -0400

    axios instance

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,37 +1,60 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
+import axiosInstance from "../../utils/axiosInstance";
 import { validateEmail } from "../../utils/helper";
-import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+  const navigate = useNavigate()
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        if (!validateEmail(email)) {
-            setError("Please enter a valid email address.");
-            return;
-          }
-      
-          if (!password) {
-            setError("Please enter the password");
-            return;
-          }
-
-          setError('')
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
     }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError('')
+
+    //Login API Call
+
+    try {
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+      });
+      
+      // Handle successful login response 
+      if(response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken)
+        navigate('/dashboard')
+      }
+
+    } catch (error) {
+      // Handle login error
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
 
   return (
     <>
-        <Navbar/>
-
-        <div className="flex items-center justify-center mt-28">
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
@@ -39,14 +62,13 @@ const Login = () => {
             <input 
               type="text" 
               placeholder="Email" 
-              className="input-box"  
+              className="input-box" 
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              />
+              onChange={(e) => setEmail(e.target.value)} />
 
-            <PasswordInput
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            <PasswordInput 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
@@ -60,7 +82,7 @@ const Login = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
