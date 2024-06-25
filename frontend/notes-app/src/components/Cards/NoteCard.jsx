18b636754f907f1bb154665bb0334b45dba1e1c3
“deep150499” <deepnanavati@icloud.com>
2024-06-25 14:10:58 -0400

    final commit

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -20,7 +20,7 @@ const NoteCard = ({title, date, content, tags, isPinned, onEdit, onDelete, onPin
       </p>
 
       <div className="flex items-center justify-between mt-2">
-        {/* <div className="text-xs text-slate-500">{tags.map((item)=> `#${item} `)}</div> */}
+        <div className="text-xs text-slate-500">{tags.map((item)=> `#${item} `)}</div>
 
         <div className="flex items-center gap-2">
           <MdCreate className="icon-btn hover:text-green-600" onClick={onEdit} />
--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,44 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from 'react';
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+  const navigate = useNavigate()
 
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-    
-        if (!name) {
-          setError("Please enter your name");
-          return;
-        }
-    
-        if (!validateEmail(email)) {
-          setError("Please enter a valid email address.");
-          return;
-        }
-    
-        if (!password) {
-          setError("Please enter the password");
-          return;
-        }
-    
-        setError('')
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name");
+      return;
     }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError('')
+
+    // SignUp API Call
+
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email: email,
+        password: password,
+      });
+      
+      // Handle successful registration response 
+      if(response.data && response.data.error){
+        setError(response.data.message)
+        return
+      }
+
+      if(response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken)
+        navigate('/dashboard')
+      }
+
+    } catch (error) {
+      // Handle login error
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
 
   return (
     <>
-        <div>
-            <Navbar/>
+      <Navbar />
 
-            <div className="flex items-center justify-center mt-28">
+      <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleSignUp}>
             <h4 className="text-2xl mb-7">Sign Up</h4>
@@ -78,9 +107,8 @@ const SignUp = () => {
           </form>
         </div>
       </div>
-        </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
