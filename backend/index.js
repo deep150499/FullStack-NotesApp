643e89fbe9365e643d4b2705db56b063369a0c3c
“deep150499” <deepnanavati@icloud.com>
2024-06-25 09:37:47 -0400

    add note API

--- backend/index.js
+++ backend/index.js
@@ -113,6 +113,44 @@ app.post("/login", async (req, res) => {
     }
   });
 
+// Add Note
+app.post("/add-note", authenticateToken, async (req, res) => {
+    const { title, content, tags } = req.body;
+    const { user } = req.user;
+  
+    if (!title) {
+      return res.status(400).json({ error: true, message: "Title is required" });
+    }
+  
+    if (!content) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Content is required" });
+    }
+  
+    try {
+      const note = new Note({
+        title,
+        content,
+        tags: tags || [],
+        userId: user._id,
+      });
+  
+      await note.save();
+  
+      return res.json({
+        error: false,
+        note,
+        message: "Note added successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal Server Error",
+      });
+    }
+  });
+
   app.listen(3000);
 
 module.exports = app;
